b52adb2d822f97ca49a957c4dcb6676c3a719f5c
Maiko Naruse <maimai@Maiko-no-MacBook-Air.local>
2018-07-24 10:01:15 +0900

    privateメソッドの追加

--- protospace_in_dev-master/app/controllers/comments_controller.rb
+++ protospace_in_dev-master/app/controllers/comments_controller.rb
@@ -2,4 +2,9 @@ class CommentsController < ApplicationController
   def create
     Comment.create(text: params[:text], prototype_id: params[:prototype_id], user_id: current_user.id))
   end
+
+  private
+  def comment_params
+    params.permit(:text, :prototype_id)
+  end
 end
