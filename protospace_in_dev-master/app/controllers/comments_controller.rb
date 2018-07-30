b8eaaf7c71c38cc48b059db4b02d7ae205d59e9b
Maiko Naruse <maimai@Maiko-no-MacBook-Air.local>
2018-07-30 14:48:23 +0900

    コントローラの設定

--- protospace_in_dev-master/app/controllers/comments_controller.rb
+++ protospace_in_dev-master/app/controllers/comments_controller.rb
@@ -1,10 +1,41 @@
 class CommentsController < ApplicationController
+  before_action :set_prototype
+  before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:create]
+
   def create
-    Comment.create(text: params[:text], prototype_id: params[:prototype_id], user_id: current_user.id))
+    @comment = Comment.create(comment_params)
+    respond_to do |format|
+      format.html { redirect_to prototype_path(@prototype.id) }
+      format.json
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @comment.update(comment_params) if @comment.user_id == current_user.id
+    redirect_to prototype_path(@prototype.id)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy if @comment.user_id == current_user.id
+    redirect_to prototype_path(@prototype.id)
   end
 
   private
+
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
   def comment_params
-    params.permit(:text, :prototype_id)
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: @prototype.id)
   end
 end
