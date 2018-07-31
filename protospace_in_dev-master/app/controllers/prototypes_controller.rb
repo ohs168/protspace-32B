d566f5b49c7c0be3e4382a708daad7f764041654
Maimai <40909634+maimaiworld@users.noreply.github.com>
2018-07-31 09:40:19 +0900

    Revert "Add comment function"

--- protospace_in_dev-master/app/controllers/prototypes_controller.rb
+++ protospace_in_dev-master/app/controllers/prototypes_controller.rb
@@ -15,14 +15,11 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to :root, notice: 'New prototype was successfully created'
     else
-      redirect_to action: 'new' , alert: 'New prototype was unsuccessfully created'
-     end
+      redirect_to ({ action: new }), alert: 'YNew prototype was unsuccessfully created'
+    end
   end
 
   def show
-   @comment = Comment.new
-   @comments = @prototype.comments.includes(:user)
-   @prototype = Prototype.find(params[:id])
   end
 
   def destroy
