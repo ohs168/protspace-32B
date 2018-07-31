273beca322afae7a4c0a8cfae54529b79246bf24
yabecchi312 <yabehideaki@MacBook-Pro-4.local>
2018-07-31 13:10:43 +0900

    Merge branch 'master' into add-like-product
    # Conflicts:
    #	protospace_in_dev-master/Gemfile
    #	protospace_in_dev-master/app/controllers/prototypes_controller.rb
    #	protospace_in_dev-master/app/models/prototype.rb
    #	protospace_in_dev-master/app/models/user.rb
    #	protospace_in_dev-master/config/routes.rb
    #	protospace_in_dev-master/db/schema.rb
    
    レイアウトを整える。

--- protospace_in_dev-master/app/controllers/likes_controller.rb
+++ protospace_in_dev-master/app/controllers/likes_controller.rb
@@ -4,70 +4,18 @@ class LikesController < ApplicationController
     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
     @likes = Like.where(prototype_id: params[:prototype_id])
     @prototypes = Prototype.all
-    @likes_count = @prototype.likes.where(prototype_id: params[:id]).count
+    @likes_count = @likes.count
   end
 
   def destroy
     like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
     like.destroy
     @likes = Like.where(prototype_id: params[:prototype_id])
-    @prototypes = Prototype.all
-    @likes_count = @prototype.likes.where(prototype_id: params[:id]).count
+    @likes_count = @likes.count
   end
  private
 
   def set_variables
     @prototype = Prototype.find(params[:prototype_id])
-    # @id_name = "#like-link-#{@prototype.id}"
-    # @id_heart = "#heart-#{@prototype.id}"
   end
 end
-
-
-
-
-
-
-# class LikesController < ApplicationController
-#   def create
-#     @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-#     @likes = Like.where(prototype_id: params[:prototype_id])
-#     respond_to do |format|
-#       format.html { redirect_to prototype_path(params[:prototype_id])  }
-#       format.json
-#     end
-#   end
-
-#   def destroy
-#     @like = current_user.likes.find_by(prototype_id: params[:prototype_id])
-#     @like.destroy
-#     @likes = Like.where(prototype_id: params[:prototype_id])
-#     espond_to do |format|
-#       format.html { redirect_to prototype_path(params[:prototype_id])  }
-#       format.json
-#     end
-#   end
-# end
-
-
-
-  # before_action :set_prototype
-
-  # def create
-  #   @like = Like.create(user_id: current_user.id, prototype_id: params[:prototype_id])
-  #   @likes = Like.where(prototype_id: params[:prototype_id])
-  #   @prototype.reload
-  # end
-
-  # def destroy
-  #   like = Like.find_by(user_id: current_user.id, prototype_id: params[:prototype_id])
-  #   like.destroy
-  #   @likes = Like.where(prototype_id: params[:prototype_id])
-  #   @prototype.reload
-  # end
-
-  # private
-
-  # def set_prototype
-  #   @prototype = Prototype.find(params[:prototype_id])
-  # end
--- protospace_in_dev-master/db/schema.rb
+++ protospace_in_dev-master/db/schema.rb
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20180724002016) do
 
   add_index "captured_images", ["prototype_id"], name: "index_captured_images_on_prototype_id", using: :btree
 
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.text     "text",         limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "likes", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
     t.integer  "user_id",      limit: 4
